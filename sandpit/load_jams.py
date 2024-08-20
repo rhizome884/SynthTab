f389c8c81b8eba06c9c67ba204d50e105c9a7db2
rhizome884 <rhizome884@gmail.com>
2024-08-20 16:38:36 +0100

    exploring jams

--- sandpit/load_jams.py
+++ sandpit/load_jams.py
@@ -1,18 +1,18 @@
 import jams
 
 # Add our custom schema for guitar notes
-jams.schema.add_namespace('gp_to_JAMS/note_tab.json')
+# jams.schema.add_namespace('gp_to_JAMS/note_tab.json')
 
-jams_file = "sandpit/jams/1 - Acoustic Steel Guitar.jams"
+jams_file = "sandpit/jams/00_BN1-129-Eb_comp.jams"
 
 data = jams.load(jams_file)
 
 # print(data)
 print(data)
 
-for anno in data["annotations"]:
-    print(anno["sandbox"])
-    for gp_data in anno["data"]:
-        print(gp_data)
-        for obs in gp_data:
-            print(obs)
+# for anno in data["annotations"]:
+#     print(anno["sandbox"])
+#     for gp_data in anno["data"]:
+#         print(gp_data)
+#         for obs in gp_data:
+#             print(obs)
